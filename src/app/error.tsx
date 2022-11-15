4c37ecaaf52733c36fa6f9f19506b8d06c25c29e
Lautaro <lautaroef@gmail.com>
2022-11-15 14:16:05 -0300

    Replced how the error was returned

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+  // useEffect(() => {
+  // Log the error to an error reporting service
+  console.error(error);
+  // }, [error]);
 
   return (
     <div style={{ color: "#000" }}>
